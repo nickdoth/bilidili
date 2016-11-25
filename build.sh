7dc0948ae4d43e4e471cc861e55702d10e528311
Nick Ng <nickdoth@NickdeMacBook-Pro.local>
2016-11-25 23:48:51 +0800

    no 'core' directory

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-pushd core/src
+pushd src
   tsc -t es5  --removeComments --lib es6,dom --outDir ../lib -m commonjs video2.ts control.ts
 popd
 
-pushd core/lib
+pushd lib
   browserify video2.js > ../built.js
   browserify control.js > ../built-control.js
 popd
 
 cat userscript.txt > built.user.js
-cat core/built.js >> built.user.js
+cat built.js >> built.user.js
 cat foot.txt >> built.user.js
 
-cp core/built.js ./crx/content-scripts/bilidili-core.js
-cp core/built-control.js ./crx/content-scripts/bilidili-control.js
+cp built.js ./crx/content-scripts/bilidili-core.js
+cp built-control.js ./crx/content-scripts/bilidili-control.js
