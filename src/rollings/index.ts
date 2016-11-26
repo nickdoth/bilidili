41fccc0c7910bd9b263dd37ab787d212f9060cb8
Nick Ng <nickdoth@NickdeMacBook-Pro.local>
2016-11-26 17:15:50 +0800

    shorthand danmaku createor

--- src/rollings/index.ts
+++ src/rollings/index.ts
@@ -6,7 +6,19 @@ export { BilibiliDanmakuDocument } from './danmaku/bilibili';
 
 import { Media as IMedia } from './media/media';
 import { Document, Connector } from './media/connector';
+import { BilibiliDanmakuDocument } from './danmaku/bilibili';
+import { DanmakuViewer } from './danmaku/viewer';
+import config from '../config';
+
 export function connect(media: IMedia, doc: Document<any>) {
     return new Connector(media, doc);
 }
+
+export function danmaku(video: IMedia, text: string) {
+    var conn = connect(video, new BilibiliDanmakuDocument(text));
+    var viewer = new DanmakuViewer(video);
+    conn.addView(viewer);
+    return viewer;
+}
+
 export var DEBUG = 0;
