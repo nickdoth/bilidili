af4a9c81198ab580aada08a8de90cb1bbec109fa
Nick Ng <nickdoth@NickdeMacBook-Pro.local>
2016-11-26 17:43:40 +0800

    Add SVG icon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var webpack = require('webpack-stream');
+var fs = require('pn/fs');
+var svg2png = require('svg2png');
 
 var tsProject = ts.createProject('tsconfig.json');
 
@@ -21,6 +23,17 @@ gulp.task('crx', ['ts'], function() {
     ].map(streamToPromise));
 })
 
+gulp.task('icon', function() {
+    return Promise.all([
+        icon(128), icon(16), icon(19), icon(38)
+    ]);
+})
+
+function icon(size) {
+    return fs.readFile('./bilidili.svg')
+        .then((buf) => svg2png(buf, { width: size }))
+        .then(buf => fs.writeFile(`./crx/images/icon-${size}.png`, buf));
+}
 
 function streamToPromise(stream) {
     return new Promise((resolve, reject) => {
